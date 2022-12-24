7d7d080b622d793504d4a1d325407adafd2aadff
Ahmed <95902084+AlphaxHotelxMikexEchoxDelta@users.noreply.github.com>
2022-12-24 14:41:51 +0100

    Add files via upload

---
+++ Log.php
@@ -0,0 +1,18 @@
+<?php
+
+class Log
+{
+
+  public function Authentification( $ip, $user, $auth, $client )
+  {
+    $file = fopen('XXX.log','a') ;
+    $date = date('Y-m-d H:i:s') ;
+
+    fwrite( $file, "\nAuthentification[$ip][$date]['$user'][$auth][$client]" ) ;
+    fclose( $file ) ;
+
+  }
+
+}
+
+?>
